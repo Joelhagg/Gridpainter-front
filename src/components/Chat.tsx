acc96b69dce11446eb292868ddb5a90e3ca7516e
Louise Rosenqvist <93682181+lrosenqv@users.noreply.github.com>
2022-08-29 22:59:58 +0200

    update socket.io chat in room

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { socket } from "./Layout";
+import { IChatMsg } from "./models/IChatMsg";
 
 interface ICloseProps {
   closeClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
@@ -10,13 +11,15 @@ export const Chat = (Props: ICloseProps) => {
   let room = useParams();
   const [message, setMessage] = useState("");
   const [messageReceived, setMessageReceived] = useState("");
+  let recievedMessages: IChatMsg[] = []
 
-  // Lägg in props för att skicka med namnet på rummet osm man joinade
+  const userName = ""
+  // Lägg in props eller useContext för att skicka med namnet på rummet osm man joinade
   // Vi behöver även få med användaren och skicka med vårat message
   /*" props med namnet på rummet och userName"*/
 
   const sendMessage = () => {
-    socket.emit("sendMessage", { message: message, room: room.room });
+    socket.emit("sendMessage", { message: message, room: room.room, user: userName });
     console.log("room: ", room.room);
   };
 
@@ -24,24 +27,32 @@ export const Chat = (Props: ICloseProps) => {
     socket.on("receiveMessage", (message) => {
       console.log(message);
       setMessageReceived(message);
+      recievedMessages.push(message)
     });
   }, [socket]);
+
+  let renderMessages = recievedMessages.map((message, i) => {
+    return(<div key={i} className={message.userId === socket.id ? "sentMsg" : "recievedMsg"}>
+      <p className="msgUser">{message.user}</p>
+      <p className="msgText">{message.text}</p>
+    </div>)
+  })
+
   return (
     <div>
-      <br />
       <h2>In-game Chat</h2>
-      <input
-        type="text"
-        value={message}
-        onChange={(event) => setMessage(event.target.value)}
-        placeholder="message"
-      />
-      <button onClick={sendMessage}> Send </button>
+      <div className="messagesBox">
+        {renderMessages}
+      </div>
+      <div className="newMsgBox">
+        <input
+          type="text"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          placeholder="message"/>
+        <button onClick={sendMessage}> Send </button>
+      </div>
       <button onClick={Props.closeClick}>Stäng</button>
-      <br />
-      <h3>Messages</h3>
-      <p>{messageReceived}</p>
-      <br />
     </div>
   );
 };
