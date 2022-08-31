ef45c164e8c701238ba90e386425d9ffbcffa64e
joelhagg <joel.pettersson90@gmail.com>
2022-08-31 17:11:10 +0200

    update chat

--- src/components/Rooms.tsx
+++ src/components/Rooms.tsx
@@ -9,15 +9,15 @@ export const Rooms = () => {
   const navigate = useNavigate();
   const [roomName, setRoomName] = useState("");
   const [rooms, setRooms] = useState<IRoom[]>([]);
-
+  let nickname = "Louise";
   async function getRooms() {
     let response = await axios.get<IRoom[]>("http://localhost:3001/rooms");
     return response.data;
   }
 
   useEffect(() => {
+    // den här ska va kvar! Men kan få flyttas om den har en bättre plats :)
     socket.connect();
-
     getRooms().then((res) => {
       setRooms(res);
     });
@@ -32,7 +32,8 @@ export const Rooms = () => {
   const joinRoom = (roomName: any) => {
     console.log("joinRoom", roomName);
 
-    socket.emit("join", roomName);
+    socket.emit("nickname", nickname);
+    socket.emit("join", roomName, nickname);
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
