e4a43754fd770655b9d37701a1311616f99aa8dc
johanna-almqvist <94167705+johanna-almqvist@users.noreply.github.com>
2022-08-31 23:23:42 +0200

    Update Rooms.tsx

--- src/components/Rooms.tsx
+++ src/components/Rooms.tsx
@@ -24,15 +24,23 @@ export const Rooms = () => {
   }, []);
 
   const createRoom = () => {
-    socket.emit("createRoom", { name: roomName, id: socket.io.engine.id });
-    joinRoom(roomName);
+    const room = {
+      name: roomName,
+      id: socket.io.engine.id,
+      nickname: localStorage.getItem("nickname"),
+    };
+    socket.emit("createRoom", room);
     navigate(`/${roomName}`);
   };
 
   const joinRoom = (roomName: any) => {
     console.log("joinRoom", roomName);
-
-    socket.emit("join", roomName);
+    const room = {
+      name: roomName,
+      id: socket.io.engine.id,
+      nickname: localStorage.getItem("nickname"),
+    };
+    socket.emit("join", room);
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
