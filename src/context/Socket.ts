271ae6bee06d14223477467f5784fd5c57dff8cc
joelhagg <joel.pettersson90@gmail.com>
2022-09-08 14:08:17 +0200

    update socket url for heroku

--- src/context/Socket.ts
+++ src/context/Socket.ts
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import { io } from "socket.io-client";
 
-export const socket = io('http://localhost:3001')
+export const socket = io('https://gridpainter-front.herokuapp.com', {"autoConnect": false, transports : ['websocket'] })
 export const SocketContext = createContext(socket)
 
 export let user = "";
