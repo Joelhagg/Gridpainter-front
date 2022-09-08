8b8f713d52d4c12f1fc9e11ca88bff7a8daa25d8
joelhagg <joel.pettersson90@gmail.com>
2022-09-08 14:34:27 +0200

    update socket url

--- src/context/Socket.ts
+++ src/context/Socket.ts
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import { io } from "socket.io-client";
 
-export const socket = io('https://gridpainter-front.herokuapp.com', {"autoConnect": false, transports : ['websocket'] })
+export const socket = io('https://gridpainter-back.herokuapp.com/', {"autoConnect": false, transports : ['websocket'] })
 export const SocketContext = createContext(socket)
 
 export let user = "";
